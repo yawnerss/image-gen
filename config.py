e73c62dc4a8a0d7c2f71f3ca47dfd9e15a58bac7
yawnerss <haburat9@gmail.com>
2025-12-06 22:41:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ AVAILABLE_MODELS = {
     "6": {"id": "gpt_1medium", "name": "🎯 GPT-1 Medium", "desc": "Better quality"},
     "7": {"id": "flux_kontext_pro", "name": "✨ Flux Kontext Pro", "desc": "Premium quality"},
     "8": {"id": "flux_2_pro", "name": "FLUX KONTEXT PRO 2 ", "desc": "Vesion 2 of kontext pro"},
-    "9": {"id": "clipfly_2", "name": "Clipfly 2", "desc": "Clip fly version"}
+    "9": {"id": "clipfly_2", "name": "Clipfly 2", "desc": "Clip fly version"},
     "10": {"id": "midjourney_v7", "name": "MID JOURNEY V7", "desc": "MID JOURNEY V7 more detield texture"}
     
 }
@@ -52,4 +52,5 @@ DEFAULT_MODEL = "nanobanana"
 # LOGGING SETTINGS
 # ============================================================================
 LOG_LEVEL = "INFO"  # DEBUG, INFO, WARNING, ERROR, CRITICAL
+
 
